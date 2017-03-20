bdce2070e093502bf29ae9c0da0c1e2e06949f7d
Tom <tomhung87@gmail.com>
2017-03-19 22:08:21 -0700

    Add customizer for individual team members. General team members section - Title and description. Individual team member - Name, training, description, image.

--- wp-content/themes/tzuchi-theme/functions.php
+++ wp-content/themes/tzuchi-theme/functions.php
@@ -9,4 +9,132 @@ function register_my_menu() {
 }
 add_action( 'init', 'register_my_menu' );
 
+
+add_action( 'customize_register', 'create_customizer');
+function create_customizer($wp_customize){
+
+	/**********************************************************/
+	/****************** TEAM MEMEBERS PANEL *******************/
+	/**********************************************************/
+	$panel_id = 'team_members_panel';
+
+	$wp_customize->add_panel( $panel_id, array(
+ 	'priority'       => 10,
+  'capability'     => 'edit_theme_options',
+  'theme_supports' => '',
+  'title'          => __('Team Members', 'tzuchi-theme'),
+  'description'    => __('Options to add a team member', 'tzuchi-theme'),
+	));
+
+	/***********************************************/
+	/************ GENERAL SECTION ******************/
+	/***********************************************/
+
+	// Title
+	$wp_customize->add_section('team_members_general', array(
+		'title' => esc_html__('General', 'tzuchi-theme' ),
+		'priority' => 10,
+		'panel' => $panel_id
+	));
+
+	$wp_customize->add_setting('team_members_general_title', array(
+		'default' => __('Heading title', 'tzuchi-theme')
+	));
+
+	$wp_customize->add_control('team_members_general_title', array(
+		'label'    => __( esc_html__('Title:'), 'tzuchi-theme' ),
+		'section'  => 'team_members_general'
+	));
+
+	//Description
+	$wp_customize->add_setting('team_members_general_description', array(
+		'default' => __('Heading description', 'tzuchi-theme')
+	));
+
+	$wp_customize->add_control('team_members_general_description', array(
+		'label'   => __( esc_html__('Description:'), 'tzuchi-theme'),
+		'section' =>	'team_members_general'
+	));
+
+
+	/**********************************************************/
+	/****************** TEAM MEMEBER 1 *******************/
+	/**********************************************************/
+
+	$wp_customize->add_section('team_members_number1', array(
+		'title' 	 => __("Team Memember #1", 'tzuchi-theme'),
+		'priority' => 20,
+		'description' => __('Team member #1 information', 'tzuchi-theme'),
+		'panel'  => $panel_id
+	));
+
+	//Name
+	$wp_customize->add_setting('team_members_number1_name', array(
+		'default'   => 'Name'
+	));
+
+	$wp_customize->add_control('team_members_number1_name', array(
+		'label'   => __( esc_html__('Name'), 'tzuchi-theme'),
+		'section' => 'team_members_number1'
+	));
+
+	//Training
+	$wp_customize->add_setting('team_members_number1_training', array(
+		'default' => "Training qualifications"
+	));
+
+	$wp_customize->add_control('team_members_number1_training', array(
+		'label'   => __( esc_html__('Training'), 'tzuchi-theme'),
+		'section' => 'team_members_number1'
+	));
+
+	//Description
+	$wp_customize->add_setting('team_members_number1_description', array(
+		'default' => 'Description'
+	));
+
+	$wp_customize->add_control('team_members_number1_description', array(
+		'label'   => __( esc_html__('Description'), 'tzuchi-theme'),
+		'type'		=> 'textarea',
+		'section' => 'team_members_number1'
+	));
+
+	//Image
+	$wp_customize->add_setting('team_members_number1_image', array(
+		'default' => get_template_directory_uri() . '/layout/images/member-default.jpg'
+	));
+
+	$wp_customize->add_control(
+       new WP_Customize_Image_Control(
+           $wp_customize,
+           'team_members_number1_image',
+           array(
+	           'label'      => __( 'Image', 'tzuchi-theme' ),
+	           'section'    => 'team_members_number1',
+	           'settings'   => 'team_members_number1_image'
+           )
+       )
+   );
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
 ?>
--- wp-content/themes/tzuchi-theme/sections/section-team.php
+++ wp-content/themes/tzuchi-theme/sections/section-team.php
@@ -1,17 +1,17 @@
 <div class="container team-section">
 	
 	<div class="intro-header">
-		<h1>Our teammates take care to everyone!</h1>
+		<h1><?php echo get_theme_mod('team_members_general_title', 'This is a default title'); ?></h1>
 
-		<p>We provide the great healers(TCM doctors) to truly work for people, with pain and sickness in mind.</p>		
+		<p><?php echo get_theme_mod('team_members_general_description', 'This is a default description text'); ?></p>
 	</div>
 
 	<div class="team-member col-lg-3 col-sm-6">
-		<img src="<?php bloginfo('template_directory'); ?>/layout/images/DrChen1029-e1487369746917.jpg">
+		<img src="<?php echo get_theme_mod('team_members_number1_image', get_template_directory_uri() . "/layout/images/member-default.jpg"); ?>">
 		<div class="member-box">
 			<div class="description">
-				<h1 class="team-member-name">Dr. Sam Chen</h1>
-				<p class="team-member-des">Dr. TCM</p>
+				<h1 class="team-member-name"><?php echo get_theme_mod('team_members_number1_name', 'Test Name'); ?></h1>
+				<p class="team-member-des"><?php echo get_theme_mod('team_members_number1_training', 'Test Training'); ?></p>
 			</div>
 		</div>
 	</div>
