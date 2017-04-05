0c78fee85aee58430b5572acbdd3f03855229c9f
Tom <tomhung87@gmail.com>
2017-04-04 22:33:16 -0700

    Add page template for News.

---
+++ wp-content/themes/tzuchi-theme/page-templates/news-page.php
@@ -0,0 +1,28 @@
+
+<?php 
+
+	/* Template Name: News Page */ 
+
+	get_header();
+
+?>
+
+	<div class="container content-wrap news-page-container">
+		<?php 
+		if( have_posts() ):
+			while( have_posts() ): the_post(); ?>
+				
+				<div class="row">
+					<div class="col-xs-12">
+						Header
+					</div>
+					<div class="col-xs-12">
+						<?php the_content()?>		
+					</div>
+				</div>
+			<?php endwhile; 
+		endif;
+	?>
+	</div>
+
+<?php get_footer(); ?>
