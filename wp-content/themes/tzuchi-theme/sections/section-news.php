239ecf11cb3f7a3d863d23fcb90daa867700decb
Tom <tomhung87@gmail.com>
2017-04-05 21:36:25 -0700

    Add services section on front page. Add additional carousel for services images.

--- wp-content/themes/tzuchi-theme/sections/section-news.php
+++ wp-content/themes/tzuchi-theme/sections/section-news.php
@@ -1,6 +1,6 @@
-<div class="container-fluid content-wrap news-section">
+<div class="container content-wrap news-section">
 	<div class="row">
-		<div class="col-xs-7 latest-post-wrap">
+		<div class="col-md-7 latest-post-wrap">
 			<?php 
 				$args = array( 'numberposts' => 1, 'post_status'=>"publish",'post_type'=>"post",'orderby'=>"post_date");
 					$post_array = get_posts( $args );
@@ -16,19 +16,66 @@
 						</header>
 						<p class="post-date"><?php echo mysql2date('F m, Y', $post->post_date) ?></p>
 						<section>
-							<p class="post-content"><?php echo $post->post_content ?></p>
+							<p class="post-content"><?php echo substr($post->post_content, 0, 500) . "..." ?></p>
 						</section>
+						<p class="read-full-post text-center">
+							<a href="<?php the_permalink(); ?>">Read More</a>
+						</p>
 					</article>
 			<?php endforeach ?>
 		</div>
+		<div class="col-md-5 services-sidebar-wrap">
+			<h1>Services</h1>
+			<div class="services-flex-wrap">
+				<div id="services-carousel" class="carousel slide" data-ride="carousel">
+			  <!-- Indicators -->
+			  <ol class="carousel-indicators">
+			    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			  </ol>
 
-		<div class="col-xs-5 news-sidebar-wrap">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner" role="listbox">
+			    <div class="item active">
+			      <img src="http://localhost/tzuchi/wp-content/uploads/2017/04/TCM-in-Burnaby-Hospital_front-window_Page_1-1.jpg" alt="...">
+			    </div>
+			    <div class="item">
+			      <img src="http://tzuchicanada.org/tcmc/wp-content/uploads/2017/02/TCM-in-Burnaby-Hospital_front-window_Page_2.jpg" alt="...">
+			    </div>
+			  </div>
+
+			  <!-- Controls -->
+			  <a class="left carousel-control" href="#services-carousel" role="button" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#services-carousel" role="button" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				<!-- <figure>
+					<img src="http://localhost/tzuchi/wp-content/uploads/2017/04/TCM-in-Burnaby-Hospital_front-window_Page_1-1.jpg">
+				</figure> -->
+				<!-- <figure>
+					<img src="http://tzuchicanada.org/tcmc/wp-content/uploads/2017/02/TCM-in-Burnaby-Hospital_front-window_Page_2.jpg">
+				</figure> -->
+			</div>
 		</div>
 	</div>
 </div>
