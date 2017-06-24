e021d8f676b2689a1adf9c305b7a25d9ac394501
Tom <tom@tomhung.ca>
2017-06-23 20:10:16 -0700

    Add Call To Action section to front-page

--- sections/section-appointment-cta.php
+++ sections/section-appointment-cta.php
@@ -1,7 +1,10 @@
 
 <div class="content-wrap cta-section">
-  <div class="appointment-cta">
-    <a href="<?php echo get_page_link(101); ?>
-">Make an Appointment</a>
-  </div>
+  <a href="<?php echo get_page_link(101); ?>
+">
+    <img src="<?php bloginfo('template_directory'); ?>/layout/images/calender-img.png" alt="calender icond" />
+  </a>
+
+  <p>Book now for your <span>free</span> consultation!</p>
+
 </div>
